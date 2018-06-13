ecb0242c3d3d74767b9465dc0a8e53aeaee58ef6
Neil Brubacher <neil.brubacher@hotmail.com>
2018-06-12 23:14:09 -0400

    Adds responsive FitChart for tuning

--- app/src/main/java/com/example/neil/tunedownforwhat/TuningActivity.java
+++ app/src/main/java/com/example/neil/tunedownforwhat/TuningActivity.java
@@ -1,22 +1,37 @@
 package com.example.neil.tunedownforwhat;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.SeekBar;
 import android.widget.Switch;
+import android.widget.TextView;
+
+import com.txusballesteros.widgets.FitChart;
 
 public class TuningActivity extends AppCompatActivity {
 
+    private int MAX_SEEK = 9;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Initialize activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tuning);
         setTitle("Tuner");
 
+        // Set controls
         final Switch toggleSwitch = findViewById(R.id.toggleSwitch);
+        final SeekBar slider = findViewById(R.id.seekBar);
+        final TextView progressDisplay = findViewById(R.id.textBox);
+        final FitChart chart = findViewById(R.id.fitChart);
 
+        // Configure controls
+        slider.setMax(MAX_SEEK);
+
+        // Set listeners
         toggleSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
 
             @Override
@@ -29,5 +44,40 @@ public class TuningActivity extends AppCompatActivity {
             }
         });
 
+        slider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar slider, int newValue, boolean fromUser) {
+                progressDisplay.setText(Integer.toString(newValue));
+                setOnChart(chart, slider.getProgress());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar slider) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar slider) {
+            }
+        });
+
+    }
+
+    private void setOnChart(FitChart chart, int inputVal) {
+
+        double median = 4.5;
+        double percentStep = 25.0 / median;
+
+        double indicator = (inputVal % 10) - median;
+        double percentProgress = 50.0 + (indicator * percentStep);
+        double offBy = Math.abs(percentProgress - 50.0);
+
+        int color;
+        if (offBy < 1 * percentStep) color = Color.parseColor("#00cc00");
+        else if (offBy < 2 * percentStep) color = Color.parseColor("#bfff00");
+        else if (offBy < 3 * percentStep) color = Color.parseColor("#ffbf00");
+        else if (offBy < 4 * percentStep) color = Color.parseColor("#ff8000");
+        else color = Color.parseColor("#ff4000");
+
+        chart.setValue((float) percentProgress, color);
     }
 }
